8a9edbdc20eb3336c10a06aeadbd671e6687adcd
Siobhan Vander Pallen <108106935+MaameSem@users.noreply.github.com>
2022-08-10 22:37:55 -0400

    Create arraysum.java

---
+++ arraysum.java
@@ -0,0 +1,17 @@
+public class Arraysum
+{
+public static void main (String[]args)
+{
+double prices[] = { 100,  40, 76, 25 };
+
+double total= 0;
+
+for (int i=0; i < prices.length; i++)
+{
+total += prices[i];
+}
+
+System.out.println("The total of the array is " + total);
+
+}
+}
